1d9aff167f164a5ab159686922199b70f273e76d
pythonthm <pythonthm@gmail.com>
2023-05-25 13:07:35 +0900

    init

---
+++ js/main.js
@@ -0,0 +1,79 @@
+// const open = document.getElementById("open");
+// const overlay = document.querySelector(".overlay");
+// const close = document.getElementById("close");
+
+// open.addEventListener("click", () => {
+//   overlay.classList.add("show");
+//   open.classList.add("hide");
+//   console.log("click");
+// });
+
+// close.addEventListener("click", () => {
+//   overlay.classList.remove("show");
+//   open.classList.remove("hide");
+// });
+
+// bodyの背景を変えるスクリプト
+document.getElementById("target1").addEventListener("click", function () {
+  document.getElementById("bodyTarget").style.background = "pink";
+});
+document.getElementById("target2").addEventListener("click", function () {
+  document.getElementById("bodyTarget").style.background = "orange";
+});
+document.getElementById("target3").addEventListener("click", function () {
+  document.getElementById("bodyTarget").style.background = "orchid";
+});
+document.getElementById("target4").addEventListener("click", function () {
+  document.getElementById("bodyTarget").style.background = "tomato";
+});
+document.getElementById("target5").addEventListener("click", function () {
+  document.getElementById("bodyTarget").style.background = "skyblue";
+});
+document.getElementById("target6").addEventListener("click", function () {
+  document.getElementById("bodyTarget").style.background = "wheat";
+});
+document.getElementById("target7").addEventListener("click", function () {
+  document.getElementById("bodyTarget").style.background = "forestgreen";
+});
+document.getElementById("target8").addEventListener("click", function () {
+  document.getElementById("bodyTarget").style.background = "coral";
+});
+document.getElementById("target9").addEventListener("click", function () {
+  document.getElementById("bodyTarget").style.background = "lightblue";
+});
+document.getElementById("target10").addEventListener("click", function () {
+  document.getElementById("bodyTarget").style.background = "greenyellow";
+});
+
+// ヘッダーの背景を変えるスクリプト
+// bodyの背景を変えるスクリプト
+document.getElementById("target11").addEventListener("click", function () {
+  document.getElementById("headTarget").style.background = "pink";
+});
+document.getElementById("target12").addEventListener("click", function () {
+  document.getElementById("headTarget").style.background = "orange";
+});
+document.getElementById("target13").addEventListener("click", function () {
+  document.getElementById("headTarget").style.background = "orchid";
+});
+document.getElementById("target14").addEventListener("click", function () {
+  document.getElementById("headTarget").style.background = "tomato";
+});
+document.getElementById("target15").addEventListener("click", function () {
+  document.getElementById("headTarget").style.background = "skyblue";
+});
+document.getElementById("target16").addEventListener("click", function () {
+  document.getElementById("headTarget").style.background = "wheat";
+});
+document.getElementById("target17").addEventListener("click", function () {
+  document.getElementById("headTarget").style.background = "forestgreen";
+});
+document.getElementById("target18").addEventListener("click", function () {
+  document.getElementById("headTarget").style.background = "coral";
+});
+document.getElementById("target19").addEventListener("click", function () {
+  document.getElementById("headTarget").style.background = "lightblue";
+});
+document.getElementById("target20").addEventListener("click", function () {
+  document.getElementById("headTarget").style.background = "greenyellow";
+});
